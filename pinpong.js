830c49eb38546f5a529a67b508ea8697580fdac6
Maria Felix <168776360+DudaF3lix@users.noreply.github.com>
2024-06-27 17:41:36 -0300

    cria bolinha 27/06/2024

--- pinpong.js
+++ pinpong.js
@@ -1,4 +1,9 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
 function setup(){
+    
     //aqui vou criar "mesa"
     createCanvas(600,400);
 }
@@ -6,4 +11,11 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     background(178,34,34);
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha)
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha, tamBolinha){
+    circle (xBolinha, yBolinha, tamBolinha)
 }
